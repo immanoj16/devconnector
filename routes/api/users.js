373739dc9dff360ef43cbc5ddd8a93fb562afb34
Manoj Kumar Maharana <manoj@triveous.com>
2018-05-13 10:42:24 +0530

    Add: user register with body parser

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../../models/User');
+
 /**
  * @route   GET /api/users/test
  * @desc    Test the Users
@@ -9,5 +11,16 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
   msg: 'users work'
 }));
+
+/**
+ * @route   GET /api/users/register
+ * @desc    Registration of User
+ * @acess   Public
+ */
+router.post('/register', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  });
+});
 
 module.exports = router;
