54fe46a22bdae8e9c7b88b013eff25a9e9d946ca
Manoj Kumar Maharana <manoj@triveous.com>
2018-05-15 20:45:46 +0530

    Add: profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile Model
+const Profile = require('../../models/Profile');
+// Load User model
+const User = require('../../models/User');
 
 /**
  * @route   GET /api/profile/test
@@ -9,5 +16,35 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
   msg: 'profile work'
 }));
+
+/**
+ * @route   GET /api/profile
+ * @desc    Get user profile
+ * @acess   Private
+ */
+router.get('/', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+/**
+ * @route   POST /api/profile
+ * @desc    Create user profile
+ * @acess   Private
+ */
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -11,15 +11,6 @@ const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
 /**
- * @route   GET /api/users/test
- * @desc    Test the Users
- * @acess   Public
- */
-router.get('/test', (req, res) => res.json({
-  msg: 'users work'
-}));
-
-/**
  * @route   GET /api/users/register
  * @desc    Registration of User
  * @acess   Public
