f16d5e270591063e1ca6d2bbfd1b8c3aa9e1b625
Manoj Kumar Maharana <manoj@triveous.com>
2018-05-16 23:25:45 +0530

    Add: add delete routes experience, education and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -234,6 +234,61 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
       profile.save()
         .then(profile => res.json(profile));
     })
+});
+
+/**
+ * @route   DELETE /api/profile/experience/:exp_id
+ * @desc    delete experience from profile
+ * @acess   Private
+ */
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      // Get remove index
+      const removeIndex = profile.experience.map(item => item.id)
+        .indexOf(req.params.exp_id)
+
+      profile.experience.splice(removeIndex, 1);
+      profile.save()
+        .then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+})
+
+/**
+ * @route   DELETE /api/profile/education/:edu_id
+ * @desc    delete education from profile
+ * @acess   Private
+ */
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      // Get remove index
+      const removeIndex = profile.education.map(item => item.id)
+        .indexOf(req.params.edu_id)
+
+      profile.education.splice(removeIndex, 1);
+      profile.save()
+        .then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+/**
+ * @route   DELETE /api/profile
+ * @desc    delete profile and user
+ * @acess   Private
+ */
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findByIdAndRemove({ user: req.user.id })
+    .then(() => {
+      User.findByIdAndRemove({ _id: req.user.id })
+        .then(() => res.json({ success: true }))
+        .catch(err => res.json(400).json(err))
+    })
+    .catch(err => res.json(400).json(err))
 })
 
 module.exports = router;
