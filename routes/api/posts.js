7fd3e0340dae2586b87a1bb14aa81676c1ce87b7
Manoj Kumar Maharana <manoj@triveous.com>
2018-05-19 20:33:19 +0530

    Add to create comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -130,4 +130,36 @@ router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req,
     .catch(err => res.status(404).json({ profilenofound: 'No profile found' }));
 });
 
+/**
+ * @route   POST /api/posts/comment/:id
+ * @desc    unlike a post
+ * @acess   Private
+ */
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // Check valid or not
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Post.findById(req.params.id)
+    .then(post => {
+
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      };
+
+      post.comment.unshift(newComment);
+
+      post.save()
+        .then(post => res.json(post));
+    })
+    .catch(err => res.status(404).json({ postnofound: 'No post found' }));
+});
+
 module.exports = router;
